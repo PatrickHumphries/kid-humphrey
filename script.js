84a21df1a395da86f1107b320c2f5016c3a88e4c
patrick humphries <patrickhumphries95@gmail.com>
2021-09-28 13:09:20 -0400

    figuring out using firebase as a backend

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const postsPromise = fetch("https://api.soundcloud.com/tracks/TRACK_ID");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ_WxXd_Zg_R3HXXhBPPkHbXJdEUbW56g",
+  authDomain: "kidhumphreymusic.firebaseapp.com",
+  projectId: "kidhumphreymusic",
+  storageBucket: "kidhumphreymusic.appspot.com",
+  messagingSenderId: "621406185602",
+  appId: "1:621406185602:web:30e4126b2c5f840c4bac9b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
